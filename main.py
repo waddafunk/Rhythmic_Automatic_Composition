49d50dca4b3a4869efc3df35834a078b151415f0
Waddafunk <jacopo.piccirillo@gmail.com>
2020-12-19 20:37:12 +0100

    pydub is real

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import os
 import pathlib
+import time
+
 import librosa
+import pydub
+from pydub import playback
 
 
 def getTracks(path):  # returns list of all wav files in "path" directory
@@ -12,13 +16,32 @@ def getTracks(path):  # returns list of all wav files in "path" directory
 projectPath = pathlib.Path(__file__).parent.absolute()  # get path of the project
 multiTrackPath = pathlib.Path(str(projectPath) + '/multitrack/lofi_1')  # append desired multitrack path
 tracks = getTracks(multiTrackPath)
-audioObject = {'files': [], 'samplingFreqs': []}  # contains audio files and relative sampling freqs
+librosaAudioObject = {'files': [], 'samplingFreqs': []}  # contains audio files and relative sampling freqs
+pydubTracks = []
+firstTrackPath = pathlib.Path(str(multiTrackPath) + '/{}'.format(tracks[0]))
+signal, rate = librosa.load(firstTrackPath, sr=None, mono=False)
+
+mixedAudio = pydub.AudioSegment.from_file(firstTrackPath, format='wav')  # load first track
+pydubTracks.append(mixedAudio)  # append first track
 # end init
 
 for index in enumerate(tracks):  # load tracks
     singleTrackPath = pathlib.Path(str(multiTrackPath) + '/{}'.format(index[1]))  # append single track name
-    audio, fs = librosa.load(singleTrackPath, sr=None)
-    audioObject['files'].append(audio)
-    audioObject['samplingFreqs'].append(fs)
+    audio, fs = librosa.load(singleTrackPath, sr=None, mono=False)  # load track
 
-print(audioObject)
+    # begin append and mix track
+    librosaAudioObject['files'].append(audio)
+    librosaAudioObject['samplingFreqs'].append(fs)
+    channel1 = audio[:, 0]
+
+    if index[0] != 0:
+        segment = pydub.AudioSegment.from_file(singleTrackPath, format='wav')
+        mix = pydubTracks[-1].overlay(segment)
+        pydubTracks.append(mix)
+    # end append and mix track
+
+playback._play_with_simpleaudio(pydubTracks[-1])  # play track
+time.sleep(len(pydubTracks[-1]) / 1000)  # wait for the track to end
+
+
+print(librosaAudioObject)  # print librosa object (not used for now)
