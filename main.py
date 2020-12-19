585ea6ac9028b634fcfaa5f318613d5a8abfa28d
Waddafunk <jacopo.piccirillo@gmail.com>
2020-12-19 16:21:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import os
 import pathlib
+import librosa
 
 
-def getTracks(path):
-    fullpath = str(path) + '/multitrack/lofi_1'
-    tracks = [f for f in os.listdir(fullpath) if f.endswith('.wav')]
-    return tracks
+def getTracks(path):  # returns list of all wav files in "path" directory
+    tracks_collection = [f for f in os.listdir(path) if f.endswith('.wav')]
+    return tracks_collection
 
 
-path = pathlib.Path(__file__).parent.absolute()
-tracks = getTracks(path)
-for track in tracks:
-    print(track)
+# begin init
+projectPath = pathlib.Path(__file__).parent.absolute()  # get path of the project
+multiTrackPath = pathlib.Path(str(projectPath) + '/multitrack/lofi_1')  # append desired multitrack path
+tracks = getTracks(multiTrackPath)
+audioObject = {'files': [], 'samplingFreqs': []}  # contains audio files and relative sampling freqs
+# end init
 
+for index in enumerate(tracks):  # load tracks
+    singleTrackPath = pathlib.Path(str(multiTrackPath) + '/{}'.format(index[1]))  # append single track name
+    audio, fs = librosa.load(singleTrackPath, sr=None)
+    audioObject['files'].append(audio)
+    audioObject['samplingFreqs'].append(fs)
 
-
-
-
-
+print(audioObject)
